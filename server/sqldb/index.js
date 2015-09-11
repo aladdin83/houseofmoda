2d5418879dc8f739883fde9c9316d4687d1293f6
Aladdin Al-Wattar <aladdin83@gmail.com>
2015-09-12 03:11:15 +0400

    initialized angular-fullstack

---
+++ server/sqldb/index.js
@@ -0,0 +1,20 @@
+/**
+ * Sequelize initialization module
+ */
+
+'use strict';
+
+var path = require('path');
+var config = require('../config/environment');
+
+var Sequelize = require('sequelize');
+
+var db = {
+  Sequelize: Sequelize,
+  sequelize: new Sequelize(config.sequelize.uri, config.sequelize.options)
+};
+
+// Insert models below
+db.Thing = db.sequelize.import('../api/thing/thing.model');
+
+module.exports = db;
