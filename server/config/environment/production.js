7b03b1561a164fb692a9adaa5f608191ae2d3626
Aladdin Al-Wattar <aladdin83@gmail.com>
2015-09-12 05:55:22 +0400

    added sequlize configs to production server config file

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -20,5 +20,16 @@ module.exports = {
             process.env.OPENSHIFT_MONGODB_DB_URL +
             process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/houseofmoda'
+  },
+  
+  sequelize: {
+    uri: 'sqlite://',
+    options: {
+      logging: false,
+      storage: 'dev.sqlite',
+      define: {
+        timestamps: false
+      }
+    }
   }
 };
