4493fa4b8a4de7b7cf223fb5f11c9111f0483a6b
Aladdin Al-Wattar <aladdin83@gmail.com>
2015-09-13 21:49:37 +0400

    added uiGmaps and map to Locations page

--- client/app/app.js
+++ client/app/app.js
@@ -5,11 +5,19 @@ angular.module('houseofmodaApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
-])
-  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  'ui.bootstrap',
+  'uiGmapgoogle-maps'
+]).config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyBMykWOQWL5I4lYyM2u8bZM4BGglFWH7aI',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+}).config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
   });
+
+
--- client/app/locations/locations.controller.js
+++ client/app/locations/locations.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('houseofmodaApp')
-  .controller('LocationsCtrl', function($scope, $http) {
-    
+  .controller('LocationsCtrl', function($scope, $http, uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.then(function(maps) {
+      $scope.locations = [
+        {
+        id: 1,
+        coords: {latitude:25.3853094, longitude:55.4757881}
+      },{
+        id: 2,
+        coords: {latitude:25.2352343, longitude:55.4360223}
+      }
+      ];
+      $scope.map = { center: { latitude: 25.3853094, longitude: 55.4757881}, zoom: 8 };
+    });
   });
